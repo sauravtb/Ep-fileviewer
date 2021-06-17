4463eb0399decce81590eb9ce0deb75ab497c623
Saurav <saurav@thinkbridge.in>
2021-06-17 20:20:37 +0530

    [setup]

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,29 +1,7 @@
-import React, { useState } from "react";
-import { ParentDiv, LoginDiv, ButtonDiv } from "./Login.styles.js";
+import React from "react";
 
 function Login() {
-  const [email, setEmail] = useState("Saurav");
-  const [password, setPassword] = useState("");
-
-  return (
-    <ParentDiv>
-      <LoginDiv>
-        <input
-          type="email"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        ></input>
-        <input
-          type="text"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-          password={password}
-        ></input>
-        <ButtonDiv>Login</ButtonDiv>
-      </LoginDiv>
-    </ParentDiv>
-  );
+  return <h1>Hello from App</h1>;
 }
 
 export default Login;
